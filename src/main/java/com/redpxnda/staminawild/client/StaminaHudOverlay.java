d42c65b5801496053a28e31ae9e4b2ae9ee579d0
grame <grames8888@gmail.com>
2022-09-26 16:28:31 -0400

    commit

--- src/main/java/com/redpxnda/staminawild/client/StaminaHudOverlay.java
+++ src/main/java/com/redpxnda/staminawild/client/StaminaHudOverlay.java
@@ -1,26 +1,14 @@
 package com.redpxnda.staminawild.client;
 
-import com.mojang.authlib.minecraft.client.MinecraftClient;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Quaternion;
-import com.mojang.math.Vector3f;
-import com.redpxnda.staminawild.StaminaWild;
 import com.redpxnda.staminawild.config.ClientConfig;
 import com.redpxnda.staminawild.config.CommonConfig;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.client.gui.IIngameOverlay;
-import net.minecraftforge.client.gui.OverlayRegistry;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import org.lwjgl.system.CallbackI;
 
 import java.lang.reflect.Array;
 
@@ -32,93 +20,184 @@ public class StaminaHudOverlay {
     private static final ResourceLocation FULL_BAR_H = new ResourceLocation("staminawild", "textures/stamina/bar/full_h.png");
     private static final ResourceLocation EMPTY_BAR_V = new ResourceLocation("staminawild", "textures/stamina/bar/empty_v.png");
     private static final ResourceLocation FULL_BAR_V = new ResourceLocation("staminawild", "textures/stamina/bar/full_v.png");
-    private static final ResourceLocation[] EGG_STAMINA = new ResourceLocation[23];
-    private static final PoseStack RENDER_REVERTED = new PoseStack();
+    private static final ResourceLocation EGG_STAMINA = new ResourceLocation("staminawild", "textures/stamina/eggs.png");
 
-    static {
-        EGG_STAMINA[0] = new ResourceLocation("staminawild", "textures/stamina/eggs/00.png");
-        for (int i = 1; i <= 22; i++) {
-            EGG_STAMINA[i] = new ResourceLocation("staminawild", "textures/stamina/eggs/0" + i + ".png");
-        }
-    }
+
+    //main method for rendering
     public static final IIngameOverlay HUD_STAMINA = ((gui, poseStack, partialTick, width, height) -> {
-        if (ClientConfig.RENDER_MODE.get().equals("Segments") || ClientConfig.RENDER_MODE.get().equals("s")) {
-            int x = width / 2;
-            int y = height;
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            int simpleStamina = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / (ClientConfig.SEGMENTS.get()*22)) - 1;
-            for (int i = 1; i <= ClientConfig.SEGMENTS.get(); i++) {
-                int i2 = i;
-                if (i*22 >= simpleStamina) {
-                    RenderSystem.setShaderTexture(0, EGG_STAMINA[1]);
-                    //poseStack.translate(0,0,-5);
-                    GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get() + (i * 23)-23, y - ClientConfig.POSITION_Y.get(), 0, 0, 22, 9, 22, 9);
-                }
-                if (((i+1)*22 > simpleStamina && i*22 < simpleStamina) || (i*22 > simpleStamina && i<2)) {
-                    int progress = simpleStamina - (i*22);
-                    if (simpleStamina < 22) {
-                        progress+=22;
-                        i2--;
+        int x;
+        int y;
+        int mode = 1;
+        switch (ClientConfig.RENDER_MODE.get()) {
+            case "s", "Segments", "eggs", "Seggments" -> {
+                switch (ClientConfig.POSITION_PRESET.get()) {
+                    case "bottom_right" -> {
+                        x = width-25;
+                        y = height-15;
+                        mode = -1;
                     }
-                    RenderSystem.setShaderTexture(0, EGG_STAMINA[progress]);
-                    System.out.println(progress);
-                    //poseStack.translate(0,0,5);
-                    GuiComponent.blit(poseStack, x-ClientConfig.POSITION_X.get()+(i2*23), y - ClientConfig.POSITION_Y.get(), 0, 0, 22, 9, 22, 9);
+                    case "bottom_left" -> {
+                        x = 3;
+                        y = height-15;
+                    }
+                    case "top_right" -> {
+                        x = width-25;
+                        y = 15;
+                        mode = -1;
+                    }
+                    case "top_left" -> {
+                        x = 3;
+                        y = 15;
+                    }
+                    default -> {
+                        x = width / 2;
+                        y = height;
+                    }
                 }
-                if (i*22 <= simpleStamina) {
-                    RenderSystem.setShaderTexture(0, EGG_STAMINA[22]);
-                    GuiComponent.blit(poseStack, x-ClientConfig.POSITION_X.get()+(i*23)-23, y - ClientConfig.POSITION_Y.get(), 0, 0, 22, 9, 22, 9);
+                x+=ClientConfig.POSITION_X.get();
+                y+=ClientConfig.POSITION_Y.get();
+                eggRendering(gui, poseStack, partialTick, x, y, mode);
+            }
+            case "hb", "Horizontal Bar", "bar" -> {
+                switch (ClientConfig.POSITION_PRESET.get()) {
+                    case "bottom_right" -> {
+                        x = width-5-ClientConfig.BAR_LENGTH.get();
+                        y = height-25;
+                    }
+                    case "bottom_left" -> {
+                        x = 3;
+                        y = height-25;
+                    }
+                    case "top_right" -> {
+                        x = width-5-ClientConfig.BAR_LENGTH.get();
+                        y = 25;
+                    }
+                    case "top_left" -> {
+                        x = 3;
+                        y = 25;
+                    }
+                    default -> {
+                        x = width / 2;
+                        y = height;
+                    }
                 }
-
+                x+=ClientConfig.POSITION_X.get();
+                y+=ClientConfig.POSITION_Y.get();
+                horizontalBarRendering(gui, poseStack, partialTick, x, y);
             }
-        } else if (ClientConfig.RENDER_MODE.get().equals("Horizontal Bar") || ClientConfig.RENDER_MODE.get().equals("hb")) {
-            int x = width / 2;
-            int y = height;
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShaderTexture(0, EMPTY_BAR_H);
-            poseStack.translate(0,0,-5);
-            GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get());
-            RenderSystem.setShaderTexture(0, FULL_BAR_H);
-            int divideAmount = CommonConfig.MAX_STAMINA.get()/ClientConfig.BAR_LENGTH.get();
-            GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientStaminaData.getPlayerStamina()/divideAmount, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get());
-        } else if (ClientConfig.RENDER_MODE.get().equals("Vertical Bar") || ClientConfig.RENDER_MODE.get().equals("vb")) {
-            int x = width / 2;
-            int y = height;
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            RenderSystem.setShaderTexture(0, EMPTY_BAR_V);
-            poseStack.translate(0,0,-5);
-            GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get());
-            poseStack.translate(0,0,5);
-            RenderSystem.setShaderTexture(0, FULL_BAR_V);
-            int divideAmount = CommonConfig.MAX_STAMINA.get()/ClientConfig.BAR_LENGTH.get();
-            GuiComponent.blit(RENDER_REVERTED, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientConfig.BAR_WIDTH.get(), ClientStaminaData.getPlayerStamina()/divideAmount, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get());
-        } else {
-            int x = width / 2;
-            int y = height;
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            double staminaFull = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10F);
-            double staminaEmpty = Math.round(staminaFull * 2.0) / 2.0;
-            staminaFull = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10F);
-            int simpleStamina = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10);
-            for (double i = 0; i < staminaFull; i++) {
-                RenderSystem.setShaderTexture(0, FILLED_STAMINA);
-                int i2 = (int) i;
-                GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get() + (i2 * 8), y - ClientConfig.POSITION_Y.get(), 0, 0, 9, 9, 9, 9);
-            }
-            for (double i = staminaFull; i < 10; i++) {
-                RenderSystem.setShaderTexture(0, EMPTY_STAMINA);
-                int i2 = (int) i;
-                GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get() + (i2 * 8), y - ClientConfig.POSITION_Y.get(), 0, 0, 9, 9, 9, 9);
-            }
-            if (Math.abs(staminaFull - simpleStamina) > 0.4) {
-                RenderSystem.setShaderTexture(0, HALF_STAMINA);
-                int iFull = (int) staminaFull;
-                GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get() + (iFull * 8), y - ClientConfig.POSITION_Y.get(), 0, 0, 9, 9, 9, 9);
+            default -> {
+                switch (ClientConfig.POSITION_PRESET.get()) {
+                    case "bottom_right" -> {
+                        x = width-25;
+                        y = height-15;
+                        mode = -1;
+                    }
+                    case "bottom_left" -> {
+                        x = 3;
+                        y = height-15;
+                    }
+                    case "top_right" -> {
+                        x = width-25;
+                        y = 15;
+                        mode = -1;
+                    }
+                    case "top_left" -> {
+                        x = 3;
+                        y = 15;
+                    }
+                    default -> {
+                        x = width / 2;
+                        y = height;
+                    }
+                }
+                lightningBoltRendering(gui, poseStack, partialTick, x, y, mode);
             }
         }
     });
+    private static void eggRendering(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int x, int y, int mode) {
+        // V to make eggs anchor to the right on right presets
+        x-=23*mode;
+        float stamina = (ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / (ClientConfig.SEGMENTS.get() * 22f)) - 1) / 22f;
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1, 1, 1, 1);
+
+        for (int segment = 1; segment <= ClientConfig.SEGMENTS.get(); segment++) {
+            int textureX;
+            if (segment <= stamina) {
+                textureX = 21*22;
+            }
+            else if (segment > stamina + 1) {
+                textureX = 0;
+            }
+            else {
+                textureX = (int)(Math.ceil((stamina - (int)stamina) * 22)) * 22 - 22;
+                if(ClientStaminaData.getPlayerStamina() == CommonConfig.MAX_STAMINA.get()) textureX = 21*22;
+            }
+            RenderSystem.setShaderTexture(0, EGG_STAMINA);
+            GuiComponent.blit(poseStack, x + (segment * (23*mode)), y, textureX, 0, 22, 9, 484, 9);
+        }
+    }
+    private static void horizontalBarRendering(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int x, int y) {
+        //Horizontal bar rendering is a rendering type to make the stamina appear in a bar across the screen.
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        // empty bar texture
+        RenderSystem.setShaderTexture(0, EMPTY_BAR_H);
+        // making sure it renders behind
+        poseStack.translate(0,0,-5);
+        // actually creating it
+        GuiComponent.blit(poseStack, x, y, 0, 0, ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get());
+        // set the texture to the full bar
+        RenderSystem.setShaderTexture(0, FULL_BAR_H);
+        // getting the divide amount- I need this so players can customize the actual bar to their heart's content
+        int divideAmount = CommonConfig.MAX_STAMINA.get()/ClientConfig.BAR_LENGTH.get();
+        // anndd the blit.
+        GuiComponent.blit(poseStack, x, y, 0, 0, ClientStaminaData.getPlayerStamina()/divideAmount, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get());
+    }
+    private static void verticalBarRendering(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int width, int height) {
+        //TODO: fix this
+        int x = width / 2;
+        int y = height;
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, EMPTY_BAR_V);
+        poseStack.translate(0,0,-5);
+        GuiComponent.blit(poseStack, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get(), ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get());
+        poseStack.translate(0,0,5);
+        RenderSystem.setShaderTexture(0, FULL_BAR_V);
+        int divideAmount = CommonConfig.MAX_STAMINA.get()/ClientConfig.BAR_LENGTH.get();
+        //GuiComponent.blit(RENDER_REVERTED, x - ClientConfig.POSITION_X.get(), y - ClientConfig.POSITION_Y.get(), 0, 0, ClientConfig.BAR_WIDTH.get(), ClientStaminaData.getPlayerStamina()/divideAmount, ClientConfig.BAR_WIDTH.get(), ClientConfig.BAR_LENGTH.get());
+    }
+    private static void lightningBoltRendering(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int x, int y, int mode) {
+        //Lightning bolt rendering is the default renderer, with vanilla style icons to indicate the player's stamina.
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        //StaminaFull is for full lightning bolt icons
+        double staminaFull = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10F);
+        //StaminaEmpty is for empty lightning bolt icons
+        double staminaEmpty = Math.round(staminaFull * 2.0) / 2.0;
+        staminaFull = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10F);
+        //SimpleStamina gets the player's stamina, without decimals
+        int simpleStamina = ClientStaminaData.getPlayerStamina() / (CommonConfig.MAX_STAMINA.get() / 10);
+        //Looping 10 times to generate all 10 icons
+        for (double i = 0; i < staminaFull; i++) {
+            RenderSystem.setShaderTexture(0, FILLED_STAMINA);
+            int i2 = (int) i;
+            GuiComponent.blit(poseStack, x+(i2 * (8*mode)), y, 0, 0, 9, 9, 9, 9);
+        }
+        for (double i = staminaFull; i < 10; i++) {
+            RenderSystem.setShaderTexture(0, EMPTY_STAMINA);
+            int i2 = (int) i;
+            GuiComponent.blit(poseStack, x+(i2 * (8*mode)), y, 0, 0, 9, 9, 9, 9);
+        }
+        //Checking if it should be a halved icon or not
+        if (Math.abs(staminaFull - simpleStamina) > 0.4) {
+            RenderSystem.setShaderTexture(0, HALF_STAMINA);
+            int iFull = (int) staminaFull;
+            GuiComponent.blit(poseStack, x+(iFull * (8*mode)), y, 0, 0, 9, 9, 9, 9);
+        }
+        //----> END RESULT <----
+        //10 lightning bolt icons in a style similar to vanilla hearts
+    }
 }
--- src/main/java/com/redpxnda/staminawild/config/ClientConfig.java
+++ src/main/java/com/redpxnda/staminawild/config/ClientConfig.java
@@ -11,6 +11,7 @@ public class ClientConfig {
     public static final ForgeConfigSpec.ConfigValue<Integer> BAR_WIDTH;
     public static final ForgeConfigSpec.ConfigValue<Integer> POSITION_X;
     public static final ForgeConfigSpec.ConfigValue<Integer> POSITION_Y;
+    public static final ForgeConfigSpec.ConfigValue<String> POSITION_PRESET;
 
     static {
         BUILDER.push("Client Config for Stamina of the Wild");
@@ -20,13 +21,15 @@ public class ClientConfig {
         SEGMENTS = BUILDER.comment("Amount of segments for the 'Segments' render mode.")
                 .define("Segments", 5);
         BAR_LENGTH = BUILDER.comment("Length of the bar (for both Horizontal and Vertical modes) in pixels.\nOnly change this if you changed the bar texture.")
-                .define("Bar Length", 200);
+                .define("Bar Length", 100);
         BAR_WIDTH = BUILDER.comment("Width of the bar (for both Horizontal and Vertical modes) in pixels.\nOnly change this if you changed the bar texture.")
                 .define("Bar Width", 20);
-        POSITION_X = BUILDER.comment("X offset of the render. (Horizontally)\nCalculated by: SCREENWIDTH/2-X")
-                .define("X Offset", 94);
-        POSITION_Y = BUILDER.comment("Y offset of the render. (Vertically)\nCalculated by: SCREENHEIGHT-Y")
-                .define("Y Offset", 54);
+        POSITION_PRESET = BUILDER.comment("Premade presets to help anchor your render to parts of the screen.\nAvailable presets: 'bottom_left', 'bottom_right', 'top_left', 'top_right', 'custom'")
+                .define("Position Preset", "bottom_left");
+        POSITION_X = BUILDER.comment("X offset of the render. (Horizontally)\nNegative values move to the left, positive move to the right.")
+                .define("X Offset", 0);
+        POSITION_Y = BUILDER.comment("Y offset of the render. (Vertically)\nNegative values move down, positive move up.")
+                .define("Y Offset", 0);
 
 
         BUILDER.pop();
